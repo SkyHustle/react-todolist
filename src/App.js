8d1c37ded9db59c41c3d34e3346467cc7cab4e52
Dmitry Sky <dmitry.sky@icloud.com>
2018-02-12 18:45:07 -0700

    validate property type for AddTodo component, refactor AddTodo unit test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddTodo from './components/AddTodo/';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">App</h1>
       </header>
+      <h1>Todo List</h1>
+      <AddTodo submitTodo={()=> {}}/>
     </div>
   );
 }
